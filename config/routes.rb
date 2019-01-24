893e7f06e7fa32a50cb816bed92ae0ae6df73df2
PGYoshi <raitei5555@gmail.com>
2019-01-24 12:27:08 +0000

    finish

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  # resources :books
+
+    root 'books#top'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
